00014ed5ea41699bc386737ce96abca12a2e7223
MusiCode1 <MusiCode3@gmail.com>
2021-02-03 12:46:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const yemot_router = require("yemot-router");
+const express = require("express");
+const port = 3000;
+
+const app = express();
+
+const y = yemot_router();
+
+y.add_fn("/", async (call) => {
+
+    let massage = [{ type: "text", data: "היי, תקיש 10" }];
+    let r = await call.read(massage);
+
+    if(r.hangup) {
+        console.log("hangup!!");
+
+        return;
+    }
+
+    massage = [
+        { type: "text", data: "הקשת " + r.data },
+        { type: "text", data: "אנא הקלט את הרחוב בו אתה גר" }
+    ];
+    r = await call.read(massage, "record");
+
+    if(r.hangup) {
+        return;
+    }
+
+    massage = [{ type: "text", data: "אנא אמור את שם הרחוב בו אתה גר" }];
+    r = await call.read(massage, "stt");
+
+    if(r.hangup) {
+        return;
+    }
+
+    massage = [{ type: "text", data: "אמרת" }];
+    r = await call.id_list_message(massage);
+
+    if(r.hangup) {
+        return;
+    }
+});
+
+app.use("/", y);
+
+app.listen(port, () => {
+    console.log("lisen in port", port);
+});
